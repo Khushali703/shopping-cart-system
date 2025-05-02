8842457445956bea8feaa57044950477c1933c99
Khushali703 <tankkhushali1@gmail.com>
2025-05-02 20:38:18 +0530

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,27 +4,11 @@ import "./index.css";
 import App from "./routes/App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Cart from "./routes/cart.jsx";
-import Items from "./components/Items.jsx";
-import PlaceOrder from "./components/placeOrder.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/home", element: <Items /> },
-      { path: "/cart", element: <Cart /> },
-      { path: "/placeOrder", element: <PlaceOrder /> },
-    ],
-  },
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
+      <App />
     </Provider>
   </StrictMode>
 );
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,18 +1,61 @@
 import "../App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
-import { Outlet } from "react-router-dom";
-function App() {
+import Items from "../components/Items";
+import Cart from "../components/cart";
+import PlaceOrder from "../components/placeOrder";
+
+const App = () => {
   return (
     <>
-      <div className="container-fluid">
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </div>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            elements={
+              <>
+                <Navbar />
+                <Items />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/home"
+            elements={
+              <>
+                <Navbar />
+                <Items />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/cart"
+            elements={
+              <>
+                <Navbar />
+                <Cart />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/placeOrder"
+            elements={
+              <>
+                <Navbar />
+                <PlaceOrder />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
